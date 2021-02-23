53caddfa268e2496d62835be9843be055637bba6
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-02-23 20:15:20 +0300

    feat (gendiff.js): add argeuments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,6 @@ import program from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .parse(process.argv);

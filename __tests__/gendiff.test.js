918f748bfced343d61fd59b7096a9f55f9708eb3
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-02-25 23:28:46 +0300

    refactor (__fixtures__/): separate flat and recursive

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 import gendiff from '../src/gendiff.js';
 import getFixturePath from '../src/utils.js';
 
-const expectedFlat = fs.readFileSync(getFixturePath('expected_flat.txt'), 'utf8');
+const expectedFlat = fs.readFileSync(getFixturePath('/flat/expected.txt'), 'utf8');
 
-const jsonFlatFilePath1 = getFixturePath('flat1.json');
-const jsonFlatFilePath2 = getFixturePath('flat2.json');
+const jsonFlatFilePath1 = getFixturePath('/flat/file1.json');
+const jsonFlatFilePath2 = getFixturePath('/flat/file2.json');
 
-const ymlFlatFilePath1 = getFixturePath('flat1.yml');
-const ymlFlatFilePath2 = getFixturePath('flat2.yml');
+const ymlFlatFilePath1 = getFixturePath('/flat/file1.yml');
+const ymlFlatFilePath2 = getFixturePath('/flat/file2.yml');
 
 test('gendiff-flat', () => {
   expect(gendiff(jsonFlatFilePath1, jsonFlatFilePath2)).toBe(expectedFlat);

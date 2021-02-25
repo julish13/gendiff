3989a92e30a86a4cc8abd3bc6a4832bf7ea6afbb
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-02-25 19:54:30 +0300

    refactor (utils.js): separate getFixturePath function

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,11 +1,6 @@
 import fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import gendiff from '../src/gendiff.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+import getFixturePath from '../src/utils.js';
 
 const path1 = getFixturePath('file1.json');
 const path2 = getFixturePath('file2.json');
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+
+export default getFixturePath;

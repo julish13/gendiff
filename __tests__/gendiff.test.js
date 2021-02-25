80c8d302c3ecc214292ef4b747e689f946688c1e
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-02-25 22:46:52 +0300

    chore: rename

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,10 +2,10 @@ import fs from 'fs';
 import gendiff from '../src/gendiff.js';
 import getFixturePath from '../src/utils.js';
 
-const path1 = getFixturePath('file1.json');
-const path2 = getFixturePath('file2.json');
+const path1 = getFixturePath('flat1.json');
+const path2 = getFixturePath('flat2.json');
 
-const result = fs.readFileSync(getFixturePath('expected_file1.txt'), 'utf8');
+const result = fs.readFileSync(getFixturePath('expected_flat.txt'), 'utf8');
 
 test('gendiff', () => {
   expect(gendiff(path1, path2)).toBe(result);

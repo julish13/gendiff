dd83ee2ca206261fb35815107caf3fb4c0ae1115
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-02-26 18:21:08 +0300

    feat: formatting function

--- src/format.js
+++ src/format.js
@@ -1,17 +1,31 @@
 const PREFIXES = {
   deleted: '  - ',
   added: '  + ',
-  same: '    ',
+  equal: '    ',
+};
+
+const formatinner = (diffs) => {
+  const diffsFormatted = diffs
+    .map(({ key, value, status }) => {
+      if(!(value instanceof Array)) {
+       return `${PREFIXES[status]}${key}: ${value}`
+      } else {
+       return `${PREFIXES[status]}${key}: {
+${formatinner(value)}
+}`
+
+      }
+    })
+    .join('\n');
+  return diffsFormatted
+  ;
 };
 
 const format = (diffs) => {
-  const diffsFormatted = diffs
-    .map(({ key, value, status }) => `${PREFIXES[status]}${key}: ${value}`)
-    .join('\n');
   return `
 {
-${diffsFormatted}
-}`;
-};
+${formatinner(diffs)}
+}`
+}
 
 export default format;

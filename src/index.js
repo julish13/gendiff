4d9b2f1ec63d62f2569d334b84759957f24ab048
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-24 00:13:07 +0300

    refactor (src/index.js): separate getFileData function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import fs from 'fs';
 import path from 'path';
-import buildAST from './buildAST/buildAST.js';
+import buildAST from './buildAST.js';
 import formatter from './formatters/index.js';
 import parse from './parsers.js';
 
+const getFileData = (filepath) => {
+  const data = fs.readFileSync(path.resolve(filepath));
+  const format = path.extname(filepath);
+  return parse(data, format);
+};
+
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const object1 = parse(
-    fs.readFileSync(path.resolve(filepath1)),
-    path.extname(filepath1),
-  );
-  const object2 = parse(
-    fs.readFileSync(path.resolve(filepath2)),
-    path.extname(filepath2),
-  );
+  const object1 = getFileData(filepath1);
+  const object2 = getFileData(filepath2);
   return formatter(buildAST(object1, object2), format);
 };
 

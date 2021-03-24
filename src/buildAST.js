649eaf50549c7a657dd8e3b8825e7c913b38a916
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-24 14:51:03 +0300

    fix (buildAST.js): unallowed reassignment

--- src/buildAST.js
+++ src/buildAST.js
@@ -21,14 +21,14 @@ const buildNode = (object1, object2) => _.sortBy(_.union(
     }
 
     const nodeChanged = { key, type: 'changed' };
-    if (_.has(object1, key)) {
-      nodeChanged.oldValue = object1[key];
-    }
 
-    if (_.has(object2, key)) {
-      nodeChanged.newValue = object2[key];
+    if (!_.has(object2, key)) {
+      return { ...nodeChanged, oldValue: object1[key] };
     }
-    return nodeChanged;
+    if (!_.has(object1, key)) {
+      return { ...nodeChanged, newValue: object2[key] };
+    }
+    return { ...nodeChanged, oldValue: object1[key], newValue: object2[key] };
   });
 
 const buildAST = (object1, object2) => ({

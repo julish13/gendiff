7810903a40ee4e609dd51bc45c8d45367c7fe341
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-31 20:31:49 +0300

    refactor (stylish.js): remove makeRecord function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,22 +1,14 @@
 import _ from 'lodash';
 
-const PREFIXES = {
-  removed: '  - ',
-  added: '  + ',
-  unchanged: '    ',
-};
-
 const makeIndent = (level, string = ' ', indentWidth = 4) => string.repeat(indentWidth).repeat(level);
 
 const stringifyValue = (value, level, cb) => ((!_.isPlainObject(value))
   ? value
   : `{
 ${_.sortBy(Object.entries(value))
-    .map(([childKey, childValue]) => cb({ key: childKey, value: childValue }, level + 1))
+    .map(([childKey, childValue]) => cb({ key: childKey, value: childValue, type: 'unchanged' }, level + 1))
     .join('\n')}
 ${makeIndent(level)}}`);
-
-const makeRecord = ({ key, value }, level, type = 'unchanged') => `${makeIndent(level - 1)}${PREFIXES[type]}${key}: ${stringifyValue(value, level, makeRecord)}`;
 
 const formatter = (node, level = 0) => {
   const { key, type } = node;
@@ -28,13 +20,15 @@ const formatter = (node, level = 0) => {
       }{
 ${node.children.map((child) => formatter(child, level + 1)).join('\n')}
 ${makeIndent(level)}}`;
+    case 'removed':
+      return `${makeIndent(level - 1)}  - ${key}: ${stringifyValue(node.value, level, formatter)}`;
+    case 'added':
+      return `${makeIndent(level - 1)}  + ${key}: ${stringifyValue(node.value, level, formatter)}`;
     case 'updated':
-      return [
-        makeRecord({ key, value: node.oldValue }, level, 'removed'),
-        makeRecord({ key, value: node.newValue }, level, 'added'),
-      ].join('\n');
+      return `${makeIndent(level - 1)}  - ${key}: ${stringifyValue(node.oldValue, level, formatter)}
+${makeIndent(level - 1)}  + ${key}: ${stringifyValue(node.newValue, level, formatter)}`;
     default:
-      return makeRecord(node, level, type);
+      return `${makeIndent(level - 1)}    ${key}: ${stringifyValue(node.value, level, formatter)}`;
   }
 };
 

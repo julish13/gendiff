83bfb46e78cfda1d2554d0ba36e709b208f29100
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-04-05 22:35:42 +0300

    refactor (stylish.js): reduce the indent value

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const makeIndent = (level, string = ' ', indentWidth = 4) => string.repeat(indentWidth).repeat(level);
+const makeIndent = (level, string = ' ', indentWidth = 2) => string.repeat(indentWidth).repeat(level);
 
 const stringifyValue = (value, level, cb) => ((!_.isPlainObject(value))
   ? value
@@ -8,27 +8,27 @@ const stringifyValue = (value, level, cb) => ((!_.isPlainObject(value))
 ${_.sortBy(Object.entries(value))
     .map(([childKey, childValue]) => cb({ key: childKey, value: childValue, type: 'unchanged' }, level + 1))
     .join('\n')}
-${makeIndent(level)}}`);
+${makeIndent(2 * level)}}`);
 
 const formatter = (node, level = 0) => {
   const { key, type } = node;
 
   switch (type) {
     case 'nested':
-      return `${makeIndent(level)}${
+      return `${makeIndent(2 * level)}${
         key === 'root' ? '' : `${key}: `
       }{
 ${node.children.map((child) => formatter(child, level + 1)).join('\n')}
-${makeIndent(level)}}`;
+${makeIndent(2 * level)}}`;
     case 'removed':
-      return `${makeIndent(level - 1)}  - ${key}: ${stringifyValue(node.value, level, formatter)}`;
+      return `${makeIndent(2 * level - 1)}- ${key}: ${stringifyValue(node.value, level, formatter)}`;
     case 'added':
-      return `${makeIndent(level - 1)}  + ${key}: ${stringifyValue(node.value, level, formatter)}`;
+      return `${makeIndent(2 * level - 1)}+ ${key}: ${stringifyValue(node.value, level, formatter)}`;
     case 'updated':
-      return `${makeIndent(level - 1)}  - ${key}: ${stringifyValue(node.oldValue, level, formatter)}
-${makeIndent(level - 1)}  + ${key}: ${stringifyValue(node.newValue, level, formatter)}`;
+      return `${makeIndent(2 * level - 1)}- ${key}: ${stringifyValue(node.oldValue, level, formatter)}
+${makeIndent(2 * level - 1)}+ ${key}: ${stringifyValue(node.newValue, level, formatter)}`;
     default:
-      return `${makeIndent(level - 1)}    ${key}: ${stringifyValue(node.value, level, formatter)}`;
+      return `${makeIndent(2 * level - 1)}  ${key}: ${stringifyValue(node.value, level, formatter)}`;
   }
 };
 
